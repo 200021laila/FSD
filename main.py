0a567f91757cc7a0aaab99d6221fdf08bfcb8daf
Laila Sevim <68811391+200021laila@users.noreply.github.com>
2024-04-30 21:07:28 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-class Main:
+"""
+The University System Main Menu should allow user to select if they
+are a student or admin or exit
+"""
+
+class UniPortal:
     
+    def __init__(self):
+        pass
+
+    def main(self):
+        self.menu()
+
